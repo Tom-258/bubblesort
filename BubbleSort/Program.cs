75e5be633e4fd3fb82087c7b9fa691a2c83478c7
Tom-258 <thomasstanger0@gmail.com>
2021-05-17 09:34:34 +0100

    bubblesort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myArr = new int[] {0, 3, 1, 8, 7, 2, 5, 4, 6, 9};
+            for (var i = 0; i < myArr.Length; i++)
+            {
+                {
+                    for (var j = 0; j < myArr.Length - 2; j++)
+                    {
+                        if (myArr[j] > myArr[j + 1])
+                        {
+                            var temp = myArr[j + 1];
+                            myArr[j + 1] = myArr[j];
+                            myArr[j] = temp;
+                        }
+
+                        foreach (var number in myArr)
+                        {
+                            Console.WriteLine(number);
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine();
+
+                    }
+                }
+            }
+        }
+    }
+}
